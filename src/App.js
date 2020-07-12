a60d6478ad26315173ca6602055d95543c3b7bb9
Ruben Freitas <emanuelfreitas@gmail.com>
2020-07-12 10:51:42 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { GlobalStyle } from "./globalCSS";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <header>
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
